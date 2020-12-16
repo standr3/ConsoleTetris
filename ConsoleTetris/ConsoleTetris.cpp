4e38ef7de5fed2ae57e372d148bd45e82e049d46
Andrei Stanciu <stanciuandrei015@gmail.com>
2020-12-17 00:16:51 +0200

    Add project files.

---
+++ ConsoleTetris/ConsoleTetris.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+wstring tetromino[7];
+
+
+int main()
+{
+    
+    // Create assets
+	tetromino[0].append(L"..X...X...X...X."); // Tetronimos 4x4
+	tetromino[1].append(L"..X..XX...X.....");
+	tetromino[2].append(L".....XX..XX.....");
+	tetromino[3].append(L"..X..XX..X......");
+	tetromino[4].append(L".X...XX...X.....");
+	tetromino[5].append(L".X...X...XX.....");
+	tetromino[6].append(L"..X...X..XX.....");
+
+}
